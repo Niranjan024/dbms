2de7a0a90b4be8af31df8fdf8ab7138d5be33ced
Niranjan024 <niranjan.s.2024.csd@rajalakshmi.edu.in>
2025-07-29 11:31:02 +0530

    Create dept table.sql

---
+++ dept table.sql
@@ -0,0 +1,7 @@
+create table department_table(
+    dept_id number(6) not null,
+    dept_name varchar(20) not null,
+    manager_id number(6),
+    location_id number(4) 
+);
+desc department_table;
